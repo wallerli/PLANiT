900d78c2bbbaec15b0678684417d608c217649bc
Waller Li <lsw626313@gmail.com>
2021-03-20 20:16:47 -0500

    Create an empty ViewTaskActivity

---
+++ PLANiT/app/src/main/java/com/example/planit/ViewTaskActivity.java
@@ -0,0 +1,26 @@
+package com.example.planit;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import java.util.UUID;
+
+public class ViewTaskActivity extends AppCompatActivity {
+
+    Project project;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_task);
+
+        Intent intent = getIntent();
+        UUID task_id = UUID.fromString(intent.getStringExtra(MainActivity.VIEW_TASK_ID));
+
+        TextView textView = findViewById(R.id.text_project_id);
+        textView.setText(task_id.toString());
+    }
+}
