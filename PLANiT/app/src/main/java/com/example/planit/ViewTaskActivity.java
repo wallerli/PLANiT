a3b8c7b134a72250ea9a838e49918eba22ba5d2d
Waller Li <lsw626313@gmail.com>
2021-03-20 20:27:28 -0500

    Fix an error

--- PLANiT/app/src/main/java/com/example/planit/ViewTaskActivity.java
+++ PLANiT/app/src/main/java/com/example/planit/ViewTaskActivity.java
@@ -10,7 +10,7 @@ import java.util.UUID;
 
 public class ViewTaskActivity extends AppCompatActivity {
 
-    Project project;
+    Task task;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
