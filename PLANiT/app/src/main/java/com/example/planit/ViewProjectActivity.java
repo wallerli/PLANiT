3b8237dae9898ddf74a06716437c91892443a5e4
Daniel Runningen <daniel.runningen+github@outlook.com>
2021-03-22 21:17:52 +0000

    Project view toolbar works

--- PLANiT/app/src/main/java/com/example/planit/ViewProjectActivity.java
+++ PLANiT/app/src/main/java/com/example/planit/ViewProjectActivity.java
@@ -1,10 +1,14 @@
 package com.example.planit;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
+
+import com.google.android.material.snackbar.Snackbar;
 
 import java.util.UUID;
 
@@ -16,6 +20,15 @@ public class ViewProjectActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_project);
+        Toolbar toolbar = findViewById(R.id.view_toolbar);
+        setSupportActionBar(toolbar);
+
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
 
         Intent intent = getIntent();
         UUID project_id = UUID.fromString(intent.getStringExtra(MainActivity.VIEW_PROJECT_ID));
