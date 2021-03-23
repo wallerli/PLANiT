78b17b196a790bdd66b625cfc07e2b8a1c61d88b
Waller Li <lsw626313@gmail.com>
2021-03-23 09:03:12 -0500

    Support get demo project and task id as string from Globals

--- PLANiT/app/src/main/java/com/example/planit/Globals.java
+++ PLANiT/app/src/main/java/com/example/planit/Globals.java
@@ -17,6 +17,9 @@ public class Globals{
     private final Map<UUID, Tag> tags = new HashMap<UUID, Tag>();
     private final Map<UUID, Task> tasks = new HashMap<UUID, Task>();
 
+    private UUID demoProjectUUID = null;
+    private UUID demoTaskUUID = null;
+
     private Globals() {
 //        projects.put(null, new Project("New Project"));
 //        tags.put(null, new Tag("New Tag"));
@@ -94,6 +97,7 @@ public class Globals{
         this.tags.clear();
         this.projects.clear();
         this.tasks.clear();
+        SimpleDateFormat df = new SimpleDateFormat("MM/dd/yyyy hh:mm", Locale.getDefault());
         Tag tag1 = new Tag("Reading", Color.rgb(255, 0, 0));
         Tag tag2 = new Tag("Do first", Color.rgb(0, 255, 0));
         Tag tag3 = new Tag("Team work");
@@ -101,23 +105,22 @@ public class Globals{
         addTag(tag2);
         addTag(tag3);
 
-        Project project1 = new Project("Sed ut perspiciatis");
+        Project project1 = new Project("P1 lus ut perspiciatis");
         project1.addTag(tag1.getUUID());
         project1.addTag(tag3.getUUID());
         project1.setText("" +
-                        "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus.\n" +
+                        "P1 lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus.\n" +
                         "\n" +
                         "Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. Phasellus viverra nulla ut metus varius laoreet. Quisque rutrum. Aenean imperdiet. Etiam ultricies nisi vel augue. Curabitur ullamcorper ultricies nisi. Nam eget dui. Etiam rhoncus. Maecenas tempus, tellus eget condimentum rhoncus, sem quam semper libero, sit amet adipiscing sem neque sed ipsum. Nam quam nunc, blandit vel, luctus pulvinar, hendrerit id, lorem. Maecenas nec odio et ante tincidunt tempus. Donec vitae sapien ut libero venenatis faucibus. Nullam quis ante."
                         );
-        SimpleDateFormat df = new SimpleDateFormat("dd/MM/yyyy hh:mm", Locale.getDefault());
         try {
-            project1.setDueDate(df.parse("2022/3/23"));
+            project1.setDueDate(df.parse("3/23/2022 23:59"));
         } catch (ParseException ignored) {}
 
-        Task task1 = new Task("Et harum quidem", Size.MEDIUM, Priority.HIGH);
+        Task task1 = new Task("T1 et harum quidem", Size.MEDIUM, Priority.HIGH);
         task1.addTag(tag1.getUUID());
         task1.setText("" +
-                        "Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem.\n" +
+                        "T1 donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem.\n" +
                         "\n" +
                         "Nulla consequat massa quis enim.\n" +
                         "\n" +
@@ -130,7 +133,7 @@ public class Globals{
         Task task2 = new Task("Itaque earum rerum hic", Size.LARGE, Priority.CRITICAL);
         task2.addTag(tag2.getUUID());
         task2.setText("" +
-                        "Curabitur ligula sapien, tincidunt non, euismod vitae, posuere imperdiet, leo. Maecenas malesuada.\n" +
+                        "T2 curabitur ligula sapien, tincidunt non, euismod vitae, posuere imperdiet, leo. Maecenas malesuada.\n" +
                         "\n" +
                         "Praesent congue erat at massa. Sed cursus turpis vitae tortor. Donec posuere vulputate arcu.\n" +
                         "\n" +
@@ -149,10 +152,10 @@ public class Globals{
                         "Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Fusce id purus. Ut varius tincidunt libero. Phasellus dolor. Maecenas vestibulum mollis" +
                         "");
         task2.addBlocker(task1.getUUID());
-        Task task3 = new Task("At vero eos et accusamus", Size.LARGE, Priority.CRITICAL);
+        Task task3 = new Task("T3 t vero eos et accusamus", Size.LARGE, Priority.CRITICAL);
         task3.addTag(tag1.getUUID());
         task3.setText("" +
-                        "Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus.\n" +
+                        "T3 am libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus.\n" +
                         "\n" +
                         "Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae.\n" +
                         "\n" +
@@ -169,20 +172,20 @@ public class Globals{
 
 
 
-        Project project2 = new Project("Quis autem vel eum iure");
+        Project project2 = new Project("P2 uis autem vel eum iure");
         project2.setText("" +
-                        "Phasellus viverra nulla ut metus varius laoreet. Quisque rutrum. Aenean imperdiet. Etiam ultricies nisi vel augue. Curabitur ullamcorper ultricies nisi. Nam eget dui. Etiam rhoncus. Maecenas tempus, tellus eget condimentum rhoncus, sem quam semper libero, sit amet adipiscing sem neque sed ipsum. Nam quam nunc, blandit vel, luctus pulvinar, hendrerit id, lorem.\n" +
+                        "P2 hasellus viverra nulla ut metus varius laoreet. Quisque rutrum. Aenean imperdiet. Etiam ultricies nisi vel augue. Curabitur ullamcorper ultricies nisi. Nam eget dui. Etiam rhoncus. Maecenas tempus, tellus eget condimentum rhoncus, sem quam semper libero, sit amet adipiscing sem neque sed ipsum. Nam quam nunc, blandit vel, luctus pulvinar, hendrerit id, lorem.\n" +
                         "\n" +
                         "Maecenas nec odio et ante tincidunt tempus. Donec vitae sapien ut libero venenatis faucibus. Nullam quis ante. Etiam sit amet orci eget eros faucibus tincidunt. Duis leo. Sed fringilla mauris sit amet nibh. Donec sodales sagittis magna. Sed consequat, leo eget bibendum sodales, augue velit cursus nunc, quis gravida magna mi a libero. Fusce vulputate eleifend sapien."
                         );
         try {
-            project2.setDueDate(df.parse("2021/3/23 5:12"));
+            project2.setDueDate(df.parse("03/23/2021 05:12"));
         } catch (ParseException ignored) {}
 
-        Task task4 = new Task("Sed ut perspiciatis unde omnis", Size.SMALL, Priority.MODERATE);
+        Task task4 = new Task("T4 ed ut perspiciatis unde omnis", Size.SMALL, Priority.MODERATE);
         task4.addTag(tag2.getUUID());
         task4.setText("" +
-                        "Lorem ipsum dolor sit amet, consectetuer adipiscing elit.\n" +
+                        "T4 orem ipsum dolor sit amet, consectetuer adipiscing elit.\n" +
                         "\n" +
                         "Aenean commodo ligula eget dolor. Aenean massa.\n" +
                         "\n" +
@@ -195,19 +198,24 @@ public class Globals{
         project2.addTask(task4.getUUID());
         addProject(project2);
         addTask(task4);
+
+
+
+        demoProjectUUID = project1.getUUID();
+        demoTaskUUID = task1.getUUID();
     }
 
     /**
-     * @return the first project UUID
+     * @return the demo project UUID as string
      */
-    public UUID getProject() {
-        return projects.keySet().iterator().next();
+    public String getProject() {
+        return demoProjectUUID.toString();
     }
 
     /**
-     * @return the first task UUID
+     * @return the demo task UUID as string
      */
-    public UUID getTask() {
-        return tasks.keySet().iterator().next();
+    public String getTask() {
+        return demoTaskUUID.toString();
     }
 }
