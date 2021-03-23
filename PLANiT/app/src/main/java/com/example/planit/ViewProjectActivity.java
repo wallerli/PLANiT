346c7f7fb2a1fa6013b983b7a86813dbcd9d8c30
Waller Li <lsw626313@gmail.com>
2021-03-23 12:34:25 -0500

    Add recyclerview to support tasks in ViewProjectActivity

--- PLANiT/app/src/main/java/com/example/planit/ViewProjectActivity.java
+++ PLANiT/app/src/main/java/com/example/planit/ViewProjectActivity.java
@@ -2,12 +2,16 @@ package com.example.planit;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.TextView;
 
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Locale;
 import java.util.UUID;
 
@@ -15,6 +19,8 @@ public class ViewProjectActivity extends AppCompatActivity {
 
     Project project;
     TextView title, due, text;
+    RecyclerView recyclerView;
+    List<UUID> tasks = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,6 +34,7 @@ public class ViewProjectActivity extends AppCompatActivity {
         Intent intent = getIntent();
         UUID project_id = UUID.fromString(intent.getStringExtra(MainActivity.VIEW_PROJECT_ID));
         project = Globals.getInstance().getProject(project_id);
+        if (project.getTasks() != null) tasks.addAll(project.getTasks());
 
         title = findViewById(R.id.projectTitleTextView);
         due = findViewById(R.id.projectDueTextView);
@@ -37,5 +44,11 @@ public class ViewProjectActivity extends AppCompatActivity {
         SimpleDateFormat df = new SimpleDateFormat("MM/dd/yyyy hh:mm", Locale.getDefault());
         due.setText(df.format(project.getDueDate()));
         text.setText(project.getText());
+
+        recyclerView = (RecyclerView) findViewById(R.id.tasksRecyclerView);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        TaskAdapter adapter = new TaskAdapter(this, tasks);
+        recyclerView.setAdapter(adapter);
     }
 }
