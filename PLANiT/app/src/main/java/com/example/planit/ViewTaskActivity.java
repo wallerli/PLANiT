39299e8a7a9cc8d746b6a4bdff6381736acf7917
Waller Li <lsw626313@gmail.com>
2021-03-23 12:24:28 -0500

    Add recyclerview to support blocker in ViewTaskActivity

--- PLANiT/app/src/main/java/com/example/planit/ViewTaskActivity.java
+++ PLANiT/app/src/main/java/com/example/planit/ViewTaskActivity.java
@@ -1,19 +1,28 @@
 package com.example.planit;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.widget.TextView;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
 
 public class ViewTaskActivity extends AppCompatActivity {
 
     Task task;
     TextView title, projectTitle, text;
+    RecyclerView recyclerView;
+    List<UUID> blockers = new ArrayList<>();
 
+    @RequiresApi(api = Build.VERSION_CODES.N)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,6 +35,7 @@ public class ViewTaskActivity extends AppCompatActivity {
         Intent intent = getIntent();
         UUID task_id = UUID.fromString(intent.getStringExtra(MainActivity.VIEW_TASK_ID));
         task = Globals.getInstance().getTask(task_id);
+        if (task.getBlockers() != null) blockers.addAll(task.getBlockers());
 
         title = findViewById(R.id.taskTitleTextView);
         projectTitle = findViewById(R.id.taskProjectTitleTextView);
@@ -34,5 +44,11 @@ public class ViewTaskActivity extends AppCompatActivity {
         title.setText(task.getTitle());
         projectTitle.setText(Globals.getInstance().getParentProject(task.getUUID()).getTitle());
         text.setText(task.getText());
+
+        recyclerView = (RecyclerView) findViewById(R.id.tasksRecyclerView);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        TaskAdapter adapter = new TaskAdapter(this, blockers);
+        recyclerView.setAdapter(adapter);
     }
 }
