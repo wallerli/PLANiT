f48f97546bd49e86be0d4cd5e2d2aaaff47ca014
Waller Li <lsw626313@gmail.com>
2021-03-24 14:22:52 -0500

    Open TaskViewActivity when clicking on recyclerview

--- PLANiT/app/src/main/java/com/example/planit/TaskViewHolder.java
+++ PLANiT/app/src/main/java/com/example/planit/TaskViewHolder.java
@@ -1,19 +1,33 @@
 package com.example.planit;
 
+import android.content.Context;
+import android.content.Intent;
 import android.view.View;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import java.util.UUID;
+
+import static com.example.planit.MainActivity.VIEW_TASK_ID;
+
 public class TaskViewHolder extends RecyclerView.ViewHolder {
 
     TextView title, description;
+    UUID taskUUID;
 
     public TaskViewHolder(@NonNull View itemView) {
         super(itemView);
 
         title = itemView.findViewById(R.id.taskTitleTextView);
         description = itemView.findViewById(R.id.taskDescriptionTextView);
+
+        itemView.setOnClickListener(v -> {
+            Context context = v.getContext();
+            Intent intent = new Intent(context, ViewTaskActivity.class);
+            intent.putExtra(VIEW_TASK_ID, taskUUID.toString());
+            context.startActivity(intent);
+        });
     }
 }
