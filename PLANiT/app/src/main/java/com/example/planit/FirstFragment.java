5c5b877b2cbfaa925dd3d1e94f96b2b560134b1f
Waller Li <lsw626313@gmail.com>
2021-03-26 11:47:53 -0500

    Add grid look to overview projects, adjust material card margins

--- PLANiT/app/src/main/java/com/example/planit/FirstFragment.java
+++ PLANiT/app/src/main/java/com/example/planit/FirstFragment.java
@@ -14,6 +14,7 @@ import android.widget.SearchView;
 import androidx.annotation.NonNull;
 import androidx.annotation.RequiresApi;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -62,7 +63,7 @@ public class FirstFragment extends Fragment {
         // Add the following lines to create RecyclerView
         recyclerView = view.findViewById(R.id.projectsRecyclerView);
         recyclerView.setHasFixedSize(true);
-        recyclerView.setLayoutManager(new LinearLayoutManager(view.getContext()));
+        recyclerView.setLayoutManager(new GridLayoutManager(view.getContext(), 2));
         showAllProjects();
 
         handler = new Handler();
