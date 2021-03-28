c4c0d723a32c7efcdcb67358719892038e01afda
Daniel Runningen <daniel.runningen+github@outlook.com>
2021-03-28 20:55:06 +0000

    Adding the text listener

--- PLANiT/app/src/main/java/com/example/planit/EditProjectActivity.java
+++ PLANiT/app/src/main/java/com/example/planit/EditProjectActivity.java
@@ -114,10 +114,37 @@ public class EditProjectActivity extends AppCompatActivity {
         timeFormat.setTimeZone(TimeZone.getTimeZone("UTC"));
         dateFormat.setTimeZone(TimeZone.getTimeZone("UTC"));
         updateChips();
-//        if (project.getTags().size() == 0)
-//            emptyTagsText.setVisibility(View.VISIBLE);
-//        else
-//            emptyTagsText.setVisibility(View.INVISIBLE);
+
+        // Listeners
+        title.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) { }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                // Add validation here
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                project.setTitle(s.toString());
+            }
+        });
+
+        text.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) { }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                // Add validation here
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                project.setText(s.toString());
+            }
+        });
 
         delete.setOnClickListener(v -> {
             AlertDialog alertDialog = new AlertDialog.Builder(this).create();
@@ -139,21 +166,6 @@ public class EditProjectActivity extends AppCompatActivity {
                 getTheme().resolveAttribute(R.attr.colorError, typedValue, true);
                 @ColorInt int color = typedValue.data;
                 b.setTextColor(color);
-            }
-        });
-
-        title.addTextChangedListener(new TextWatcher() {
-            @Override
-            public void beforeTextChanged(CharSequence s, int start, int count, int after) { }
-
-            @Override
-            public void onTextChanged(CharSequence s, int start, int before, int count) {
-                // Add validation here
-            }
-
-            @Override
-            public void afterTextChanged(Editable s) {
-                project.setTitle(s.toString());
             }
         });
     }
